5b156a2765fc9cd4bb6cf4f98a78c9497a6cb2e3
nataly <email>
2021-09-29 01:06:46 +0300

    Lesson2 homework

---
+++ src/main/java/ru/geekbrains/javacore/lesson2/homework/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.javacore.lesson2.homework;
+
+public class MyArraySizeException extends NegativeArraySizeException{
+
+    public MyArraySizeException(String s) {
+        super(s);
+    }
+
+}
